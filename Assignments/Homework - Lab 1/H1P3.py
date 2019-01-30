18cf4873ee8f5d20a7df025287c65f301f1d8512
originaloctopus <46700795+originaloctopus@users.noreply.github.com>
2019-01-29 20:50:32 -0800

    Create H1P3.py

---
+++ Assignments/Homework - Lab 1/H1P3.py
@@ -0,0 +1,48 @@
+from decimal import *
+pennies=int(input(print("How many pennies do you have?")))
+penniesVal=float(pennies*.01)
+nickels=int(input(print("How many nickels do you have?")))
+nickelsVal=float(nickels*.05)
+dimes=int(input(print("How many dimes do you have?")))
+dimesVal=float(dimes*.1) 
+quarters=int(input(print("How many quaters do you have?")))
+quartersVal=float(quarters*.25)
+halfDollars=int(input(print("How many half dollars do you have?")))
+halfDollarsVal=float(halfDollars*.5)
+oneDollars=int(input(print("How many one dollar do you have?")))
+oneDollarsVal=float(oneDollars)
+if pennies==1:
+  print ("You only have 1 penny.")
+else:
+    print ("You have ",(pennies)," pennies.")
+
+if (nickels==1):
+  print ("You only have 1 nickel.")
+else:
+    print ("You have ",nickels," nickels.")
+
+if dimes ==1:
+  print ("You only have 1 dime.")
+else:
+    print ("You have ",dimes," dimes.")
+
+if quarters==1:
+  print ("You only have 1 quarter.")
+else:
+    print ("You have ",(quarters)," quarters.")
+
+if halfDollars==1:
+  print ("You only have 1 half dollar.")
+else:
+    print ("You have ",halfDollars," half dollars.")
+
+if oneDollars==1:
+  print ("You only have 1 one dollar.")
+else:
+    print ("You have ",oneDollars," dollars.")
+
+totalMonies=Decimal()
+totalMonies.quantize(Decimal(0.01))
+totalMonies=float(penniesVal+nickelsVal+dimesVal+quartersVal+halfDollarsVal+oneDollarsVal)    
+
+print("The total of all your monies is: $",totalMonies)
